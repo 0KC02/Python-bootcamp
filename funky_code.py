9fdccede3499db2d24fa2a089a798ce5aea28b77
Kerry <kerrychun00@gmail..com>
2024-08-30 22:18:09 +0800

    funkycode

---
+++ funky_code.py
@@ -0,0 +1,33 @@
+char = "#"
+
+# Print the letter 'C'
+print(f" {char*4}")
+print(f"{char}    ")
+print(f"{char}    ")
+print(f"{char}    ")
+print(f" {char*4}")
+print("")
+
+# Print the letter 'O'
+print(f" {char*4}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f"{char}    {char}")
+print(f" {char*4}")
+print("")
+
+# Print the letter 'D'
+print(f"{char*3} ")
+print(f"{char}  {char}")
+print(f"{char}  {char}")
+print(f"{char}  {char}")
+print(f"{char*3} ")
+print("")
+
+# Print the letter 'E'
+print(f"{char*4}")
+print(f"{char}   ")
+print(f"{char*3} ")
+print(f"{char}   ")
+print(f"{char*4}")
+print("")
